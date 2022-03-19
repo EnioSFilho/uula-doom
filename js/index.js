3a6fcd2f73cacadcba26a1b5bab05e1c2000ad96
unchargedash <eniofilho76@gmail.com>
2022-03-19 12:41:11 -0300

    ate a proxima

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+// exercicio 1
+// o header deve ter a cor de fundo #2E948A.
+
+// console.log(header[0]);
+// document.getElementsByTagName('header').style.backgroundColor = '#2E948A';
+
+let header = document.getElementsByTagName('header');
+
+header[0].style.backgroundColor = '#2E948A';
+
+// exercicio 2
+
+
+let linkCursos = document.querySelector('#menu_opcoes nav a:nth-child(1)');
+
+console.log(linkCursos);
+
+linkCursos.setAttribute('href', 'cursos.html');
+
+// exercicio 3
+
+document.querySelector('titulo.depoimento h3').innerHTML=" o que falam sobre nos9 ");
+
+// EXECCICIO 4
+
+
+
+
+
+
